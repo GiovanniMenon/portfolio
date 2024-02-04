6d8fed05bc637627d6debfc417bda6fc9c3c38f5
Menny <giovanni.menon.dev@gmail.com>
2024-02-05 00:15:01 +0100

    4/02/2024 - Develop - Fix

---
+++ src/components/navBarComponent.tsx
@@ -0,0 +1,50 @@
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList, navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import Link from "next/link";
+
+
+
+function NavBarComponent() {
+    return (
+        <div className="fixed w-full flex justify-center py-4">
+            <NavigationMenu className="py-2 px-5 rounded-3xl backdrop-blur-sm border shadow-2xl">
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <Link href="" legacyBehavior passHref>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                Home
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link href="" legacyBehavior passHref>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                About Me
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link href="" legacyBehavior passHref>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                Project
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link href="" legacyBehavior passHref>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                Resumee
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+        </div>
+    )
+}
+
+export default NavBarComponent;
