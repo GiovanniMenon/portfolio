779f237ba8709ed34c3244210e416b3d4fa5f4e5
Menny <giovanni.menon.dev@gmail.com>
2024-02-04 23:41:28 +0100

    4/02/2024 - 1

--- src/components/hero/heroHeaderComponent.tsx
+++ src/components/hero/heroHeaderComponent.tsx
@@ -5,7 +5,7 @@ function HeroHeader() {
         <div className="flex flex-col justify-center items-center content-center w-full h-[60vh] ">
             {/* eslint-disable-next-line react/no-unescaped-entities */}
             <div className="max-w-[1300px] flex flex-col ">
-                <span><span className="font-bold">$</span> Hello, I&apos;m Giovanni.</span>
+                <span><span className="font-bold">$</span> Hello,  I&apos;m Giovanni.</span>
                 <span><span className="font-bold">$</span> A student passionate about Cybersecurity and Programming.</span>
             </div>
 
