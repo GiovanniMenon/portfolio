a1a54aaf59c3a139d3817825b49967ffb63e153e
Menny <giovanni.menon.dev@gmail.com>
2024-02-05 00:11:39 +0100

    4/02/2024 - Main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 "use client"
 
-
-import NavBarComponent from "@/components/navBarComponent";
 import HeroComponent from "@/components/hero/heroComponent";
 
 export default function Home() {
   return (
-      <main>
-          <NavBarComponent></NavBarComponent>
+      <main className="h-full">
           <HeroComponent></HeroComponent>
-
       </main>
   );
 }
