f76919458fe2e26fb67f4cac1d8ce540b2a9028e
Menny <giovanni.menon.dev@gmail.com>
2024-02-05 14:40:20 +0100

    5/02/2024 - Main - Fix

--- src/components/hero/heroHeaderComponent.tsx
+++ src/components/hero/heroHeaderComponent.tsx
@@ -7,7 +7,7 @@ function HeroHeader() {
             <div className="max-w-[1300px] flex flex-col h-full text-center md:text-left p-2 leading-relaxed">
                 <span>Hello there &#128075;,</span>
                 <span className="text-4xl md:text-7xl font-bold">I am <span className="text-blue-600">Giovanni.</span></span>
-                <span className="font-medium">I&apos;m hard at work creating my personal website,</span>
+                <span className="font-medium">I&apos;m working to create my personal website,</span>
                 <span>but it&apos;s currently under development.</span>
                 <span> In the meantime, feel free to connect with me on <a className="underline text-blue-600" href="https://www.linkedin.com/in/giovanni-menon/">LinkedIn</a> and explore my projects on <a className="underline text-blue-600" href="https://github.com/GiovanniMenon">GitHub</a>.</span>
             </div>
