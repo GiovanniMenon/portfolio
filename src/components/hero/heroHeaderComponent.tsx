2a0f4ce924841770376d88588a593c8b83d7cc82
Menny <giovanni.menon.dev@gmail.com>
2024-02-11 21:42:41 +0100

    11/02/2024 - Develop - Hero

--- src/components/hero/heroHeaderComponent.tsx
+++ src/components/hero/heroHeaderComponent.tsx
@@ -1,18 +1,21 @@
-
-
 function HeroHeader() {
-    return (
-        <div className="flex flex-col justify-center items-center content-center w-full h-[60vh] text-4xl">
-            {/* eslint-disable-next-line react/no-unescaped-entities */}
-            <div className="max-w-[1300px] flex flex-col ">
-                <span><span className="font-bold">$</span> Hello there,  I&apos;m Giovanni.</span>
-                <span className="text-xl"> A student passionate about Cybersecurity and Programming.</span>
-            </div>
+  return (
+    <div className="flex flex-col justify-center items-center content-center w-full h-[60vh] text-4xl">
+      {/* eslint-disable-next-line react/no-unescaped-entities */}
+      <div className="max-w-[1300px] flex flex-col ">
+        <span>
+          <span className="font-bold">$</span> Hello there,{" "}
+          <span>I am Giovanni.</span>
+        </span>
+        <span className="text-xl">
+          {" "}
+          A student passionate about Cybersecurity and Programming.
+        </span>
+      </div>
 
-            {/* Possibile uso di una animazione*/}
-        </div>
-    )
-
+      {/* Possibile uso di una animazione*/}
+    </div>
+  );
 }
 
 export default HeroHeader;
