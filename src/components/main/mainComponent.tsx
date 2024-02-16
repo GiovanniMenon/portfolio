c6b30fb5822f954e20d6274d5c58967d09e3035d
Menny <giovanni.menon.dev@gmail.com>
2024-02-16 21:05:14 +0100

    16/02/2024 - Develop - Tracing

--- src/components/main/mainComponent.tsx
+++ src/components/main/mainComponent.tsx
@@ -4,26 +4,28 @@ import Footer from "@/components/footer/footer";
 import GridSkill from "@/components/main/technlogies/gridSkill";
 import ProjectGrid from "@/components/main/project/projectGridCompoment";
 import ExperienceGrid from "@/components/main/experience/experienceGrid";
-
+import { TracingBeam } from "@/components/ui/tracing-beam";
 function MainComponent() {
   const text = `
         I am a 21-year-old Italian student deeply passionate about technology and innovation. 
         Currently, I am in my third year pursuing a Bachelor's degree in Computer Science (Department of Mathematics) at the University of Padua.`;
 
   return (
-    <div className="max-w-[1300px] mx-auto p-[25px] flex flex-col md:gap-20">
-      <MainItemComponent id="aboutMe" header="About Me">
-        <p className="text-zinc-400">{text}</p>
-        <GridSkill></GridSkill>
-      </MainItemComponent>
-      <MainItemComponent id="experience" header="Experience">
-        <ExperienceGrid></ExperienceGrid>
-      </MainItemComponent>
-      <MainItemComponent id="project" header="Project">
-        <ProjectGrid />
-      </MainItemComponent>
-      <Footer></Footer>
-    </div>
+    <TracingBeam className="">
+      <div className=" max-w-[1300px] mx-auto  p-[25px] flex flex-col md:gap-20 antialiasedrelative">
+        <MainItemComponent id="aboutMe" header="About Me">
+          <p className="text-zinc-400">{text}</p>
+          <GridSkill></GridSkill>
+        </MainItemComponent>
+        <MainItemComponent id="experience" header="Experience">
+          <ExperienceGrid></ExperienceGrid>
+        </MainItemComponent>
+        <MainItemComponent id="project" header="Project">
+          <ProjectGrid />
+        </MainItemComponent>
+        <Footer></Footer>
+      </div>
+    </TracingBeam>
   );
 }
 
