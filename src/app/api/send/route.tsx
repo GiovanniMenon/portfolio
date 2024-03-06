a66ece7bd65f5108edf88d26bcdb570d1c9d52e1
Menny <giovanni.menon.dev@gmail.com>
2024-03-06 15:33:39 +0100

    06/03/2024 - Develop - EmailBody

--- src/app/api/send/route.tsx
+++ src/app/api/send/route.tsx
@@ -1,6 +1,7 @@
 import { Resend } from "resend";
 import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
+import { Separator } from "@/components/ui/separator";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -12,11 +13,23 @@ export async function POST(req: NextRequest) {
       from: "Acme <onboarding@resend.dev>",
       to: [process.env.EMAIL_TO_SEND!, email],
       subject: subject,
-      react: <div>{body}</div>,
+      react: (
+        <div>
+          <p>{body}</p>
+          <br />
+          <br />
+          <p>
+            This email was sent via cc to: {email} and
+            giovanni.menon15@gmail.com{" "}
+          </p>
+        </div>
+      ),
     });
 
+    console.log(data);
     return NextResponse.json({ body: data }, { status: 200 });
   } catch (error) {
+    console.log(error);
     return NextResponse.json({ body: error }, { status: 500 });
   }
 }
