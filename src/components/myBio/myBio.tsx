c3d9b196316fbd9e4906dda11586a6093278d9c2
Menny <giovanni.menon.dev@gmail.com>
2024-09-02 12:27:14 +0200

    Add tooltip

--- src/components/myBio/myBio.tsx
+++ src/components/myBio/myBio.tsx
@@ -6,6 +6,11 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   Code,
   Gamepad,
+  Instagram,
+  InstagramIcon,
+  Linkedin,
+  LinkedinIcon,
+  LucideLinkedin,
   MailIcon,
   MapPinIcon,
   Music,
@@ -14,7 +19,18 @@ import {
   Music4,
   University,
 } from "lucide-react";
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import {
+  DiscordLogoIcon,
+  GitHubLogoIcon,
+  InstagramLogoIcon,
+  LinkedInLogoIcon,
+} from "@radix-ui/react-icons";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 function MyBio() {
   const githubLink = "https://github.com/GiovanniMenon";
@@ -122,117 +138,88 @@ function MyBio() {
       <Separator className={"h-[2px] rounded opacity-75"} />
       <CardFooter className={"flex justify-center items-center h-full py-3.5"}>
         <div className={"flex gap-x-3"}>
-          <Button
-            variant={"secondary"}
-            className={"rounded-full"}
-            size={"icon"}
-          >
-            <Link href={githubLink} rel="noopener noreferrer" target="_blank">
-              <svg
-                className="with-icon_icon__MHUeb"
-                data-testid="geist-icon"
-                fill="none"
-                height="24"
-                shapeRendering="geometricPrecision"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="1.5"
-                viewBox="0 0 24 24"
-                width="24"
-                style={{
-                  color: "var(--geist-foreground)",
-                  width: "18px",
-                  height: "18px",
-                }}
-              >
-                <path
-                  d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44
-                         5.44 0 0020 4.77 5.07 5.07 0 0019.91 1S18.73.65 16 2.48a13.38 13.38 0 00-7 0C6.27.65 5.09
-                          1 5.09 1A5.07 5.07 0 005 4.77a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 009
-                           18.13V22"
-                />
-              </svg>
-            </Link>
-          </Button>
-          <Button
-            variant={"secondary"}
-            className={"rounded-full"}
-            size={"icon"}
-          >
-            <Link href={linkedinLink} rel="noopener noreferrer" target="_blank">
-              <svg
-                className="with-icon_icon__MHUeb"
-                data-testid="geist-icon"
-                fill="none"
-                height="24"
-                shapeRendering="geometricPrecision"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="1.5"
-                viewBox="0 0 24 24"
-                width="24"
-                style={{
-                  color: "var(--geist-foreground)",
-                  width: "18px",
-                  height: "18px",
-                }}
-              >
-                <path
-                  fill="var(--geist-fill)"
-                  stroke="var(--geist-fill)"
-                  d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6z"
-                />
-                <path
-                  fill="var(--geist-fill)"
-                  stroke="var(--geist-fill)"
-                  d="M2 9h4v12H2z"
-                />
-                <circle
-                  stroke="var(--geist-fill)"
-                  fill="var(--geist-fill)"
-                  cx="4"
-                  cy="4"
-                  r="2"
-                />
-              </svg>
-            </Link>
-          </Button>
-          <Button
-            variant={"secondary"}
-            className={"rounded-full"}
-            size={"icon"}
-          >
-            <Link
-              href={instagramLink}
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <svg
-                className="with-icon_icon__MHUeb"
-                data-testid="geist-icon"
-                fill="none"
-                height="24"
-                shapeRendering="geometricPrecision"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="1.5"
-                viewBox="0 0 24 24"
-                width="24"
-                style={{
-                  color: "var(--geist-foreground)",
-                  width: "18px",
-                  height: "18px",
-                }}
-              >
-                <rect x="2" y="2" width="20" height="20" rx="5" ry="5" />
-                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37z" />
-                <path d="M17.5 6.5h.01" />
-              </svg>
-            </Link>
-          </Button>
+          <TooltipProvider delayDuration={100} skipDelayDuration={1000}>
+            <Tooltip>
+              <TooltipTrigger>
+                <Link
+                  href={githubLink}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <Button
+                    variant={"secondary"}
+                    className={"rounded-full"}
+                    size={"icon"}
+                  >
+                    <GitHubLogoIcon className={"size-5"} />
+                  </Button>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Github</p>
+              </TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger>
+                <Link
+                  href={linkedinLink}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <Button
+                    variant={"secondary"}
+                    className={"rounded-full"}
+                    size={"icon"}
+                  >
+                    <LinkedInLogoIcon className={"size-5"} />
+                  </Button>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Linkedin</p>
+              </TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger>
+                <Link
+                  href={instagramLink}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <Button
+                    variant={"secondary"}
+                    className={"rounded-full"}
+                    size={"icon"}
+                  >
+                    <InstagramLogoIcon className={"size-5"} />
+                  </Button>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Instagram</p>
+              </TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger>
+                <Link
+                  href={instagramLink}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <Button
+                    variant={"secondary"}
+                    className={"rounded-full"}
+                    size={"icon"}
+                  >
+                    <DiscordLogoIcon className={"size-5"} />
+                  </Button>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Discord</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </div>
       </CardFooter>
     </Card>
